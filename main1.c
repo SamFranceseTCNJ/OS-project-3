3f6873e7eb5bcfbd818d260996e8b315affd7292
Nunziomvd <154708602+Nunziomvd@users.noreply.github.com>
2024-04-01 16:07:51 -0400

    Added Output files, Changed Program Output, Calculation of Physical Address wrong

--- main1.c
+++ main1.c
@@ -1,74 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PAGE_TABLE_SIZE 256
+#define PAGE_SIZE 256
+#define PHYSICAL_MEM_SIZE (PAGE_TABLE_SIZE * PAGE_SIZE)
+
 int* logicalAddresses;
 int* pageNumbers;
 int* offsets;
-char** physicalMem;
+char* physicalMem;
+int* pageTable;
 
+// Mask the least significant 16 bits
 int maskNum(int dec) {
-    int mask = 0xffff;
-    return (mask & dec);
+    return (dec & 0xFFFF);
 }
 
 int main(int argc, char** argv) {
     FILE* fp = fopen("addresses.txt", "r");
-    int nAddresses = 1000; //TODO find actual number of addresses in the file
+    FILE* out_f1 = fopen("out1.txt", "w"); // Open output file
+    FILE* out_f2 = fopen("out2.txt", "w"); 
+    FILE* out_f3 = fopen("out3.txt", "w"); 
+    int nAddresses = 1000; // Assume 1000 addresses, as per the provided sample
     logicalAddresses = (int*)malloc(sizeof(int) * nAddresses);
     pageNumbers = (int*)malloc(sizeof(int) * nAddresses);
     offsets = (int*)malloc(sizeof(int) * nAddresses);
-    physicalMem = (char**)malloc(sizeof(char *) * 256);
-    int pageTable[256];
-    for(int i = 0; i < 256; ++i) {
-        pageTable[i] = -1;
-        physicalMem[i] = NULL;
-    }
+    physicalMem = (char*)malloc(sizeof(char) * PHYSICAL_MEM_SIZE);
+    pageTable = (int*)malloc(sizeof(int) * PAGE_TABLE_SIZE);
 
-    for(int i = 0; i < nAddresses; i++) {
-        fscanf(fp, "%d", &logicalAddresses[i]);
+    // Initialize page table
+    for (int i = 0; i < PAGE_TABLE_SIZE; ++i) {
+        pageTable[i] = -1; // Mark all entries as invalid
     }
-    fclose(fp);
 
     FILE* backing = fopen("BACKING_STORE.bin", "rb");
-    for(int i = 0; i < nAddresses; ++i) {
-        int frameNumber;
+
+    for (int i = 0; i < nAddresses; ++i) {
+        fscanf(fp, "%d", &logicalAddresses[i]);
         logicalAddresses[i] = maskNum(logicalAddresses[i]);
-        offsets[i] = (logicalAddresses[i] & 0xff);
-        pageNumbers[i] = ((logicalAddresses[i]>>8) & 0xff); 
-        int currPage = pageNumbers[i];
+        offsets[i] = (logicalAddresses[i] & 0xFF);
+        pageNumbers[i] = ((logicalAddresses[i] >> 8) & 0xFF);
 
-        //check if page is in page table
-        //if it is, get frame number
-        frameNumber = pageTable[currPage];
-        if(frameNumber == -1) {
-            //if not, bring the page in from backing store
-            fseek(backing, currPage*256, SEEK_SET);
-            int freeFrame = currPage; //find first free frame. in this case, current page number
-            physicalMem[freeFrame] = (char *)malloc(256 * sizeof(char));
-            fread(physicalMem[freeFrame], sizeof(char), 256, backing);
+        int pageNumber = pageNumbers[i];
+        int offset = offsets[i];
 
-            pageTable[currPage] = freeFrame;
-            frameNumber = pageTable[currPage];
-        } else {
-            printf("page found\n");
+        int frameNumber = pageTable[pageNumber];
+        if (frameNumber == -1) { // Page fault
+            fseek(backing, pageNumber * PAGE_SIZE, SEEK_SET);
+            fread(physicalMem + (pageNumber * PAGE_SIZE), sizeof(char), PAGE_SIZE, backing);
+            pageTable[pageNumber] = pageNumber; // Assume demand paging, so frame number is same as page number
+            frameNumber = pageNumber;
         }
-        
-    }
-    fclose(backing);
 
-    printf("address 0 : %d\n", logicalAddresses[0]);
-    printf("    page  : %d\n", pageNumbers[0]);
-    printf("    offset: %d\n", offsets[0]);
-    printf("address 999: %d\n", logicalAddresses[999]);
-    printf("    page  : %d\n", pageNumbers[999]);
-    printf("    offset: %d\n", offsets[999]);
+        int physicalAddress = (frameNumber * PAGE_SIZE) + offset;
+        char value = physicalMem[physicalAddress];
+	
+	printf("Virtual Address: %d, Physical Address: %d, Value: %d\n", logicalAddresses[i], physicalAddress, value);
+
+        fprintf(out_f1,"%d\n", logicalAddresses[i]);
+        fprintf(out_f2,"%d\n", physicalAddress);
+        fprintf(out_f3,"%d\n", value);
+    }
+
+    fclose(fp);
+    fclose(backing);
+    fclose(out_f1); // Close the output file
 
     free(logicalAddresses);
     free(pageNumbers);
     free(offsets);
-    for(int i = 0; i < 256; ++i) {
-        free(physicalMem[i]);
-    }
     free(physicalMem);
+    free(pageTable);
+
     return 0;
 }
+
+
